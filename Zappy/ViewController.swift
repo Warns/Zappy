a1ceb04d1468134ecc57bc1f57fcea0d81eea254
Umid Alnuaimi <Warns@Umids-MacBook-Pro.local>
2017-03-31 22:33:30 +0300

    My first commit

--- Zappy/ViewController.swift
+++ Zappy/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func internetFor(_ sender: Any) {
+        
+        coolLabel.text = "Hello there!"
+    }
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func changeBtnTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >= 10 {
+            coolLabel.text = ("You tapped too much")
+            
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
