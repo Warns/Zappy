e8dbf06252347fe22f5024071c90cebcf08f7411
Omeed Muhammed <Warns@umids-macbook-pro.local>
2017-03-31 20:30:58 +0300

    Initial Commit

---
+++ Zappy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zappy
+//
+//  Created by Umid Alnuaimi on 31/03/17.
+//  Copyright © 2017 Umid Alnuaimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
